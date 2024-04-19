13ce6b6cdf28aad63917aa6c2a176afa8bf91de2
dietervansteenwegen <github@vansteenwegen.org>
2024-04-19 11:56:16 +0200

    src is not a package so __init__.py isn't run

--- {{cookiecutter.repo_bare}}/src/__main__.py
+++ {{cookiecutter.repo_bare}}/src/__main__.py
@@ -8,6 +8,7 @@ __project_link__ = '{{cookiecutter.project_link}}'
 
 from config.config import Config
 from {{cookiecutter.module_name}} import {{cookiecutter.class_name}}
+from log.log import add_rotating_file, setup_logger
 {% if cookiecutter.add_gui -%}
 from gui.gui import start_gui
 
@@ -15,8 +16,12 @@ def gui():
     start_gui()
 {%- endif %}
 
+def _setup_log():
+    log = setup_logger()
+    add_rotating_file(log)
 
 def main():
+    _setup_log()
     conf = Config()
     conf.get_config()
     {% if cookiecutter.add_gui %}
