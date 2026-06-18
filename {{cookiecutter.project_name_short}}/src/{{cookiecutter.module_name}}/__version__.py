30b76669e184554e078b2b30d9a016af3bd38ede
Dieter Vansteenwegen <github@vansteenwegen.org>
2026-06-18 22:25:24 +0200

    fix: use module name from templating in __version__

--- {{cookiecutter.project_name_short}}/src/{{cookiecutter.module_name}}/__version__.py
+++ {{cookiecutter.project_name_short}}/src/{{cookiecutter.module_name}}/__version__.py
@@ -1,10 +1,10 @@
-"""Runtime package version exposed as ``pydalec.__version__``."""
+"""Runtime package version exposed as `{{cookiecutter.module_name}}.__version__`."""
 
 from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as _pkg_version
 
 try:
-    __version__ = _pkg_version("pydalec")
+    __version__ = _pkg_version(distribution_name='{{cookiecutter.module_name}}')
 except PackageNotFoundError:
     # Allows source-tree imports before the package is installed.
-    __version__ = "0.0.0+unknown"
+    __version__ = '0.0.0+unknown'
